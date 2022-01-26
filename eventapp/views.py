92419e47d9b04662cb1e9ff10b5b47b5f0a959a8
AmruthaSureshk <87410450+AmruthaSureshk@users.noreply.github.com>
2022-01-26 09:55:22 +0530

    Update views.py

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
-from datetime import datetime, timedelta, time
+from datetime import date
 
 from .models import Event
 
@@ -10,7 +10,7 @@ from .models import Event
 # Create your views here.
 
 def index(request):
-    event = Event.objects.all().order_by('startdate').filter(published=True,)
+    event = Event.objects.all().order_by('startdate').filter(published=True,startdate__range=[date.today(), "2023-12-12"],enddate__range=[date.today(),"2023-12-12"])
     p = Paginator(event, 10)
     page_number= request.GET.get('page')
     page_obj = p.get_page(page_number)
